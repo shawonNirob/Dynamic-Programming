28d0b17a28f3c1fe840ad8982756620169691782
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2023-12-04 23:40:57 +0600

    Create Longest Increasing Subsequence (LIS).java

---
+++ Longest Increasing Subsequence (LIS).java
@@ -0,0 +1,29 @@
+public class LongestIncreasingSubsequence {
+
+    public static int longestIncreasingSubsequence(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+
+        int maxLIS = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            maxLIS = Math.max(maxLIS, dp[i]);
+        }
+
+        return maxLIS;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {10, 22, 9, 33, 21, 50, 41, 60, 80};
+        System.out.println("Length of Longest Increasing Subsequence: " + longestIncreasingSubsequence(nums));
+    }
+}
