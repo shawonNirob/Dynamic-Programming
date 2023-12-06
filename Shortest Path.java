e79179249a12ac53806039d93213397bd1ae5bd6
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2023-12-06 21:55:15 +0600

    Update Shortest Path.java

--- Shortest Path.java
+++ Shortest Path.java
@@ -1,19 +1,21 @@
+package dp;
+
 import java.util.Arrays;
 
-public class ShortestPathDP {
+public class ShortestPath {
 
     // Function to find the shortest path in a graph using dynamic programming
     public static int shortestPath(int[][] graph, int source, int destination) {
-        int numVertices = graph.length;
-        int[] dp = new int[numVertices];
+        int v = graph.length;
+        int[] dp = new int[v];
         Arrays.fill(dp, Integer.MAX_VALUE);
 
         dp[source] = 0;
 
-        for (int vertex = 0; vertex < numVertices; vertex++) {
-            for (int neighbor = 0; neighbor < numVertices; neighbor++) {
-                if (graph[vertex][neighbor] != 0 && dp[vertex] != Integer.MAX_VALUE) {
-                    dp[neighbor] = Math.min(dp[neighbor], dp[vertex] + graph[vertex][neighbor]);
+        for (int i = 0; i < v; i++) {
+            for (int j = 0; j < v; j++) {
+                if (graph[i][j] != 0 && dp[i] != Integer.MAX_VALUE) {
+                    dp[j] = Math.min(dp[j], dp[i] + graph[i][j]);
                 }
             }
         }
@@ -22,24 +24,17 @@ public class ShortestPathDP {
     }
 
     public static void main(String[] args) {
-        // Example graph represented as an adjacency matrix
         int[][] graph = {
-            {0, 4, 0, 0, 0, 0, 0, 8, 0},
-            {4, 0, 8, 0, 0, 0, 0, 11, 0},
-            {0, 8, 0, 7, 0, 4, 0, 0, 2},
-            {0, 0, 7, 0, 9, 14, 0, 0, 0},
-            {0, 0, 0, 9, 0, 10, 0, 0, 0},
-            {0, 0, 4, 14, 10, 0, 2, 0, 0},
-            {0, 0, 0, 0, 0, 2, 0, 1, 6},
-            {8, 11, 0, 0, 0, 0, 1, 0, 7},
-            {0, 0, 2, 0, 0, 0, 6, 7, 0}
+                {0, 3, 1, 0},
+                {0, 0, 0, 5},
+                {0, 0, 0, 2},
+                {0, 0, 0, 0}
         };
 
         int source = 0;
-        int destination = 4;
+        int destination = 3;
 
-        int shortestDistance = shortestPath(graph, source, destination);
-
-        System.out.println("Shortest distance from " + source + " to " + destination + ": " + shortestDistance);
+        int shortestPath = ShortestPath.shortestPath(graph, source, destination);
+        System.out.println("Shortest path from " + source + " to " + destination + ": " + shortestPath);
     }
 }
